b27ac5211949f490f295d4af98a2f04b3887f450
acud <12988138+acud@users.noreply.github.com>
2024-08-15 09:17:17 -0600

    wip

--- deployment.sh
+++ deployment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y gcc docker.io neovim unzip ocl-icd-opencl-dev git-lfs make ca-certificates curl wget
+apt-get install -y gcc docker.io neovim unzip ocl-icd-opencl-dev git-lfs make ca-certificates curl wget screen
 
 curl -LO https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
+rm -rf /usr/local/go; tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
 source ~/.bashrc
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
 
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -35,15 +35,24 @@ echo "alias gd='git diff'" >>~/.bashrc
 echo "alias gl='git log'" >>~/.bashrc
 echo "alias gll='git log --no-decorate --oneline'" >>~/.bashrc
 echo "alias gca='git commit --amend'" >>~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin' >>~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin:/root/.bin' >>~/.bashrc
+mkdir ~/.bin
+
 source ~/.bashrc
 mkdir -p ~/go/src/github.com/spacemeshos
 cd ~/go/src/github.com/spacemeshos/
 git clone https://github.com/spacemeshos/go-spacemesh.git
 cd go-spacemesh
-make install && make lint && make generate && make build
+make install
+make build
 touch ~/.netrc
 cd ~
 ln -s go/src/github.com/spacemeshos/go-spacemesh smh
+mkdir -p node/data
+cp smh/build/* node/ 
+cd node
 wget https://github.com/spacemeshos/quicksync-rs/releases/download/v0.1.15/quicksync-linux-v0.1.15.zip
-unzip quicksync-linux-v0.1.15.zip
+unzip quicksync-linux-v0.1.15.zip && rm quicksync-linux-v0.1.15.zip
+curl -LO https://raw.githubusercontent.com/acud/tooling/main/run.sh
+chmod +x quicksync
+screen -L ./quicksync download --node-data ./node-data
