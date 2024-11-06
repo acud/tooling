8cc7c8c5e0e1d6b7c0b4a2a81914fd29b71eb47f
acud <12988138+acud@users.noreply.github.com>
2024-11-06 11:03:00 -0600

    wip ;

--- userspace-deployment.sh
+++ userspace-deployment.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y gcc docker.io neovim unzip ocl-icd-opencl-dev git-lfs make ca-certificates curl wget screen jq
-
-curl -LO https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-rm -rf /usr/local/go; tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
-source ~/.bashrc
-
-
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
 	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
 
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo 'export GOPATH="/root/go"' >>~/.bashrc
+sudo apt-get update
+sudo apt-get install -y gcc docker.io neovim unzip ocl-icd-opencl-dev git-lfs make ca-certificates curl wget screen jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+curl -LO https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go;sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
+source ~/.bashrc
+
+echo "export GOPATH=\"$HOME/go\"" >>~/.bashrc
 echo "alias v='nvim'" >>~/.bashrc
 echo "alias co='git checkout'" >>~/.bashrc
 echo "alias vi='nvim'" >>~/.bashrc
@@ -36,7 +31,7 @@ echo "alias gd='git diff'" >>~/.bashrc
 echo "alias gl='git log'" >>~/.bashrc
 echo "alias gll='git log --no-decorate --oneline'" >>~/.bashrc
 echo "alias gca='git commit --amend'" >>~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin:/root/.bin' >>~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin' >>~/.bashrc
 mkdir ~/.bin
 export PATH=$PATH:$GOPATH/bin:/root/.bin
 
